85564d733a4921c0cffc6ebc830f7f971110ed94
Cam <133520431+cammclain@users.noreply.github.com>
2024-04-18 16:40:52 -0500

    setting up .env

--- ReactSploit/server/services/authService.go
+++ ReactSploit/server/services/authService.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Credentials struct {
@@ -11,13 +14,28 @@ type Credentials struct {
 	Password string `json:"password"`
 }
 
+func load_config() {
+	config := godotenv.Load()
+	if config != nil {
+		panic("Error loading .env file for auth service")
+	}
+	// fmt.Println("Loaded config")
+	msfHost := os.Getenv("MSF_HOST")
+	if msfHost == "" {
+		panic("MSF_HOST environment variable not set")
+	}
+}
+
 func AuthenticateWithMSF(credentials Credentials) (bool, error) {
 	// Construct the request to msfrpc
+	load_config()
+
 	data, _ := json.Marshal(map[string]string{
 		"username": credentials.Username,
 		"password": credentials.Password,
 	})
-	resp, err := http.Post("http://localhost:55553/api/v1/auth/login", "application/json", bytes.NewBuffer(data))
+	msfHost := os.Getenv("MSF_HOST")
+	resp, err := http.Post("http://"+msfHost+":55553/api/v1/auth/login", "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		return false, err
 	}
